8784bccbfc557f1b97b34adfa43b73c93a9f0e13
teregiray <@dantonengier@mail.ru>
2023-02-14 13:54:05 +0300

    i feel bad

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,7 @@ const { Command } = require('commander');
 
 const program = new Command();
 
-// import gendiff from '../src/gendiffLogick.js'
+import gendiff from '../src/gendiffLogick.js'
 
 program
   .name('gendiff')
@@ -12,9 +12,9 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format, <type>', 'output format')
-  // .arguments('<filepath1> <filepath2>')
-  // .action((path1, path2) =>
-  //   console.log(gendiff(path1, path2)))
+  .arguments('<filepath1> <filepath2>')
+  .action((path1, path2) =>
+    console.log(gendiff(path1, path2)))
 
   program.command("gendiff")
 
--- src/gendiffLogick.js
+++ src/gendiffLogick.js
@@ -1,1 +1,47 @@
 #!/usr/bin/env node
+
+const file1Path = {
+    "host": "hexlet.io",
+    "timeout": 50,
+    "proxy": "123.234.53.22",
+    "follow": false
+  }
+
+const file2Path = {
+    "timeout": 20,
+    "verbose": true,
+    "host": "hexlet.io"
+}  
+
+const fs = require('fs');
+
+function genDiff(file1Path, file2Path) {
+
+    const data1 = JSON.parse(fs.readFileSync(file1Path, 'utf-8'));
+    const data2 = JSON.parse(fs.readFileSync(file2Path, 'utf-8'));
+    const keys = new Set([...Object.keys(data1), ...Object.keys(data2)]);
+    const diff = [];
+    for (const key of [...keys].sort()) {
+        if (!(key in data1)) {
+
+            diff.push(`+ ${key}: ${data2[key]}`);
+
+            }   else if (!(key in data2)) {
+
+                    diff.push(`- ${key}: ${data1[key]}`);
+
+            }   else if (data1[key] !== data2[key]) {
+
+                    diff.push(`- ${key}: ${data1[key]}`);
+
+                    diff.push(`+ ${key}: ${data2[key]}`);
+
+}
+
+}
+
+return diff.join('\n');
+
+};
+const result = genDiff(file1Path, file2Path);
+console.log(result)
