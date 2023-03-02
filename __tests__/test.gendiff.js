4fa4a4f68eb4eba0074667fd8cdb2ed30c7dea58
teregiray <dantonengier@mail.ru>
2023-03-02 10:04:21 +0300

    test completed

--- __tests__/test.gendiff.js
+++ __tests__/test.gendiff.js
@@ -2,19 +2,16 @@ import genDiff from '../src/gendiffLogick.js';
 import { fileURLToPath } from 'url';
 import * as path from 'path'
 import { readFileSync } from 'fs';
-import { stringify } from '../src/gendiffLogick.js';
-import exp from 'constants';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// console.log(genDiff(readFile("file1.json","file2.json")))
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 describe('genDiff', () => {
-    test("Тест 1", () => {
+    test("difference from plain objects", () => {
         const filePath1 = getFixturePath('file1.json');
         const filePath2 = getFixturePath('file2.json');
-        const expectedResult = stringify(readFile('expected_file.json'));
+        const expectedResult = readFile('expected_file.json');
         expect(genDiff(filePath1,filePath2)).toEqual(expectedResult);
     });
 });
