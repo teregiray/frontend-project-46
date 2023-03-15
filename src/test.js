0f471feac97baf5bea5521618527011eed3f22a2
Андрей Руденко <dantonengier@mail.ru>
2023-03-15 14:19:40 +0300

    move functions

--- src/test.js
+++ src/test.js
@@ -1,49 +1,9 @@
 import _ from "lodash";
 
-const stringify = (value, replacer = ' ', spacesCount = 4) => {
-    const iter = (currentValue, depth) => {
-      if (typeof currentValue !== 'object' || currentValue === null) {
-        return `${currentValue}`;
-      }
-  
-      const indentSize = depth * spacesCount;
-      const currentIndent = replacer.repeat(indentSize);
-      const bracketIndent = replacer.repeat(indentSize - spacesCount);
-      const lines = Object.entries(currentValue).map(([key, val]) => `${currentIndent}${key}: ${iter(val, depth + 1)}`);
-  
-      return [
-        '{',
-        ...lines,
-        `${bracketIndent}}`,  
-      ].join('\n');
-    };
-  
-    return iter(value, 1);
-  };
+import { plainDiff, stringify } from "./helper.js";
 
 
-const plainDiff = (data1, data2) => {
 
-    const keys1 = Object.keys(data1);
-    const keys2 = Object.keys(data2);
-    const unionKeys = _.union(keys1, keys2); 
-    const sortedKeys = _.sortBy(unionKeys)
-    const result = {};
-    for (const key of sortedKeys) {
-      if (!Object.hasOwn(data1, key)) {
-        result[`${'+'}  ${key}`] = data2[key];
-      } else if (!Object.hasOwn(data2, key)) {
-        result[`${'-'}  ${key}`] = data1[key];
-      } else if (data1[key] === data2[key]) {
-        result[`${' '}  ${key}`] = data1[key]; 
-      } else if (data1[key] !== data2[key]) {
-        result[`${'-'}  ${key}`] = data1[key];
-        result[`${'+'}  ${key}`] = data2[key];
-      } 
-    }
-  
-    return result;
-  };
 const data1 = {
     "common": {
       "setting1": "Value 1",
@@ -101,4 +61,4 @@ const data2 = {
       "fee": 100500
     }
   }
-console.log(stringify(diffVerser(data1, data2))); 
+console.log(stringify(plainDiff(data1, data2))); 
