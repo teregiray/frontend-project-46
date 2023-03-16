d4593ccb975d2150ba1783e8e52c7330e5c7a6c3
teregiray <dantonengier@mail.ru>
2023-03-16 10:54:48 +0300

    smth

--- src/helper.js
+++ src/helper.js
@@ -72,64 +72,7 @@ export const plainDiff = (data1, data2) => {
         return acc;
       }, {});
     };
-  
     return iter(data1, data2);
+    
   };
-  const data1 = {
-    "common": {
-      "setting1": "Value 1",
-      "setting2": 200,
-      "setting3": true,
-      "setting6": {
-        "key": "value",
-        "doge": {
-          "wow": ""
-        }
-      }
-    },
-    "group1": {
-      "baz": "bas",
-      "foo": "bar",
-      "nest": {
-        "key": "value"
-      }
-    },
-    "group2": {
-      "abc": 12345,
-      "deep": {
-        "id": 45
-      }
-    }
-  }
-const data2 = {
-    "common": {
-      "follow": false,
-      "setting1": "Value 1",
-      "setting3": null,
-      "setting4": "blah blah",
-      "setting5": {
-        "key5": "value5"
-      },
-      "setting6": {
-        "key": "value",
-        "ops": "vops",
-        "doge": {
-          "wow": "so much"
-        }
-      }
-    },
-    "group1": {
-      "foo": "bar",
-      "baz": "bars",
-      "nest": "str"
-    },
-    "group3": {
-      "deep": {
-        "id": {
-          "number": 45
-        }
-      },
-      "fee": 100500
-    }
-  } 
-  console.log(stringify(diffNested(data1, data2)));
+ 
