9aa6f72fba0fd317c44373da4bbc2ae9cfeb7c60
teregiray <dantonengier@mail.ru>
2023-03-28 10:54:36 +0300

    complete 6 step

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,17 +1,18 @@
 import _ from "lodash";
 
-const indent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
+const indent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount -2);
 
 const stringify = (data, depth, mapping) => {
   if (!_.isObject(data)) {
-    if (data === '') return data;
-    return ` ${String(data)}`;
+if (data === ''){ 
+  return data;
+}
+    return String(data);
   }
-
   const output = Object.entries(data)
-  .map(([key, value]) => `${mapping.unchanged({ key, value }, depth + 1)}`);
-
-return `{\n${output.join('\n')}\n  ${indent(depth)}}`;
+  .map(([key, value]) => `${mapping.unchanged({ key, value}, depth +1)}`);
+  
+  return `{\n${output.join('\n')}\n${indent(depth)}  }`;
 };
 
   const mapping = {
@@ -19,31 +20,32 @@ return `{\n${output.join('\n')}\n  ${indent(depth)}}`;
       const output = children.flatMap((node) => mapping[node.type](node, depth +1, iter));
       return `{\n${output.join('\n')}\n}`;
     },
-
-    nested: ({ key, children }, depth, iter ) => {
+    nested: ({ key, children }, depth, iter) => {
     const output = children.flatMap((node) => mapping[node.type](node, depth +1, iter));
-    return `${indent(depth)}  ${key}: {\n${output.join('\n')}\n  ${indent(depth)}}`;
+    return `${indent(depth)}  ${key}: {\n${output.join('\n')}\n${indent(depth)}  }`;
     },
-
-    added: (node , depth) => `${indent(depth)}+ ${node.key}:${stringify(node.value, depth, mapping)}`,
-    
-    deleted: (node , depth) => `${indent(depth)}- ${node.key}:${stringify(node.value, depth, mapping)}`,
-    
-    unchanged: (node , depth) => `${indent(depth)}  ${node.key}:${stringify(node.value, depth, mapping)}`,
-    
+    added: (node , depth) => `${indent(depth)}+ ${node.key}: ${stringify(node.value, depth, mapping)}`,
+    deleted: (node , depth) => `${indent(depth)}- ${node.key}: ${stringify(node.value, depth, mapping)}`,
+    unchanged: (node , depth) => `${indent(depth)}  ${node.key}: ${stringify(node.value, depth, mapping)}`,
     changed: (node, depth) => {
       const { key, value1, value2 } = node;
-      const data1 = `${indent(depth)}- ${key}:${stringify(value1, depth, mapping)}`;
-      const data2 = `${indent(depth)}+ ${key}:${stringify(value2, depth, mapping)}`;
+      
+if (stringify(value1, depth, mapping) === '')
+{
+  const data1 = `${indent(depth)}- ${key}:${stringify(value1, depth, mapping)}`;
+  const data2 = `${indent(depth)}+ ${key}: ${stringify(value2, depth, mapping)}`;
+  return [data1, data2]
+} 
 
-      return [data1, data2]
+  const data1 = `${indent(depth)}- ${key}: ${stringify(value1, depth, mapping)}`;
+  const data2 = `${indent(depth)}+ ${key}: ${stringify(value2, depth, mapping)}`;
+  return [data1, data2]
     }
   }
 
 
 const renderTree = (ast) => {
   const iter = (node, depth) =>  mapping[node.type](node, depth, iter);
-
     return iter(ast, 0)
   };
 
