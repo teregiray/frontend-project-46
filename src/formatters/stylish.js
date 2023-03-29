96db64449734e88674af190202a21913754603fd
teregiray <dantonengier@mail.ru>
2023-03-29 10:51:18 +0300

    fix

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -29,13 +29,6 @@ if (data === ''){
     unchanged: (node , depth) => `${indent(depth)}  ${node.key}: ${stringify(node.value, depth, mapping)}`,
     changed: (node, depth) => {
       const { key, value1, value2 } = node;
-      
-if (stringify(value1, depth, mapping) === '')
-{
-  const data1 = `${indent(depth)}- ${key}:${stringify(value1, depth, mapping)}`;
-  const data2 = `${indent(depth)}+ ${key}: ${stringify(value2, depth, mapping)}`;
-  return [data1, data2]
-} 
 
   const data1 = `${indent(depth)}- ${key}: ${stringify(value1, depth, mapping)}`;
   const data2 = `${indent(depth)}+ ${key}: ${stringify(value2, depth, mapping)}`;
